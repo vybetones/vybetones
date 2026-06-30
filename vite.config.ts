4f603cf570608374c8eaf61deac7cd5ffea245da
vybetones <openclawnoel77@gmail.com>
2026-06-30 21:28:34 +0300

    fix: GitHub Pages configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vybetones/',
+  base: './', // GitHub Pages не поддерживает префиксы в базовом URL
   server: {
     port: 3000,
     host: true
