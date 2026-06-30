854ad2c378e5d9af22fef88c1e5e60ab67226bec
vybetones <noel77@gmail.com>
2026-06-30 20:27:12 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-    base: '/vybetones/',
- 
+  base: '/vybetones/',
+  plugins: [react()],
 
-  plugins: [
-    react(),
-  ],
   server: {
     port: 3000,
     host: true
