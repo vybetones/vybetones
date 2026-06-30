1d0b7aebb48f282e0fe4209778ab0a38abf90109
vybetones <noel77@gmail.com>
2026-06-30 21:39:59 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // GitHub Pages не поддерживает префиксы в базовом URL
+  base: '/vybetones/',
   server: {
     port: 3000,
     host: true
