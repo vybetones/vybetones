725087285aea25a3d1fb905fa7948e45af0064ab
vybetones <openclawnoel77@gmail.com>
2026-06-30 20:16:14 +0300

    chore: set base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vybetones/',
   plugins: [react()],
-
+  base: '/vybetones/',
   server: {
     port: 3000,
     host: true
